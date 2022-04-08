637f7b96e49b8e317d003f47489d9191b5c62d79
Khang Nguyen <nguyenduykhanghuflit@gmail.com>
2022-04-08 16:05:30 +0700

    render data 1

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,30 +2,10 @@ const { response } = require('express');
 const e = require('express');
 const db = require('../models/index');
 const CRUD = require('../services/CRUD');
-import { faker } from '@faker-js/faker';
 class HomeController {
-  async home(req, res) {
+  async Index(req, res) {
     let data = await CRUD.getAllTestData();
-    //oke test
-    res.send(data);
+    res.render('home/home.ejs', { data: data });
   }
-  // async GetData(req, res) {
-  //   let data = req.body.data;
-  //   let classID = data.classID;
-  //   let subject = ['Toán', 'Vật Lý', 'Hóa Học'];
-  //   let student = await CRUD.getStudentByClassID(classID);
-  //   for (let i = 0; i < subject.length; i++) {
-  //     let sj = subject[i];
-  //     for (let j of student) {
-  //       let st = j.studentID;
-  //       let data = { subject: sj, studentID: st };
-  //       let cr = await CRUD.createScrore(data);
-  //       console.log(cr);
-  //     }
-  //   }
-
-  //   res.send('vừa chạy xong');
-  // }
 }
-
 module.exports = new HomeController();
--- src/routes/homeRouter.js
+++ src/routes/homeRouter.js
@@ -2,11 +2,6 @@ import express from 'express';
 let router = express.Router();
 const homeController = require('../controller/homeController');
 
-router.get('/', homeController.home);
-// router.post('/get-data', homeController.GetData);
-
-//thêm, hiển thị student
-
-// router.get('/showstudent', testController.ShowStudent);
+router.get('/', homeController.Index);
 
 module.exports = router;
